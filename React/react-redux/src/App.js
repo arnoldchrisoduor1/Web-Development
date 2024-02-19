836d4fb279833271af5049e9500e34df53ea37c1
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-02-19 04:12:30 +0300

    react redux crash course

---
+++ React/react-redux/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import {actions} from './store/index';
+
+
+const App = () => {
+
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch(actions.increment());
+  }
+
+  const decrement = () => {
+    dispatch(actions.decrement());
+  }
+
+  const addBy = () => {
+    dispatch(actions.addBy(10));
+  }
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add Value By 10</button>
+    </div>
+  )
+}
+
+export default App
