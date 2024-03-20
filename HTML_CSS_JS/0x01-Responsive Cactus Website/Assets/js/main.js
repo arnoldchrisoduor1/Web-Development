0ff27caf6ec85e9dc444fb61d5fdb3ea83a3fff5
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-03-20 23:04:14 +0300

    Will start on the shop section of the website

--- HTML_CSS_JS/0x01-Responsive Cactus Website/Assets/js/main.js
+++ HTML_CSS_JS/0x01-Responsive Cactus Website/Assets/js/main.js
@@ -1,11 +1,41 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
 
+/* Menu Show*/
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu Hidden */
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    // when we click on each link we remove the show menu
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () => {
+    const header = document.getElementById('header')
 
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                        : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
---
+++ HTML_CSS_JS/0x01-Responsive Cactus Website/REAME.md
@@ -0,0 +1,3 @@
+# Home Cactus Decor.
+
+This website is the landing page of a hoem decor business dealing primarily with the diferent species of cactus.
